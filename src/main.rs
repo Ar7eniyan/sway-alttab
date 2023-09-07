4070e7a12582ede8530a4159d69645739af2c427
Ar7eniyan <ar7eniy.k@gmail.com>
2023-09-07 22:59:03 +0300

    feat: forward events via a uinput device

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,36 @@ use std::io::ErrorKind;
 use evdev_rs::Device;
 use evdev_rs::ReadFlag;
 use evdev_rs::ReadStatus;
+use evdev_rs::UInputDevice;
 
 fn main() {
     let file = OpenOptions::new()
         .read(true)
         .write(true)
-        .open("/dev/input/event12")  // TODO: don't hardcode
+        .open("/dev/input/event12") // TODO: don't hardcode
         .unwrap();
-    let d = Device::new_from_file(file).unwrap();
+    let mut d = Device::new_from_file(file).unwrap();
+    d.grab(evdev_rs::GrabMode::Grab).unwrap();
+
+    let ud = UInputDevice::create_from_device(&d).unwrap();
+    println!("uinput device: {}", ud.devnode().unwrap_or("none"));
 
     loop {
         let ev = d.next_event(ReadFlag::BLOCKING);
         match ev {
-            Ok((ReadStatus::Success, ev)) => println!(
-                "Event: time {}.{}, +++ {}, {}, {} +++",
-                ev.time.tv_sec,
-                ev.time.tv_usec,
-                ev.event_type()
-                    .map(|ev_type| format!("{}", ev_type))
-                    .unwrap_or("".to_owned()),
-                ev.event_code,
-                ev.value
-            ),
+            Ok((ReadStatus::Success, ev)) => {
+                println!(
+                    "Event: time {}.{}, +++ {}, {}, {} +++",
+                    ev.time.tv_sec,
+                    ev.time.tv_usec,
+                    ev.event_type()
+                        .map(|ev_type| format!("{}", ev_type))
+                        .unwrap_or("".to_owned()),
+                    ev.event_code,
+                    ev.value
+                );
+                ud.write_event(&ev).unwrap();
+            }
             Ok((ReadStatus::Sync, ev)) => println!("Sync: {}", ev.event_type().unwrap()),
             Err(e) => {
                 if e.kind() != ErrorKind::WouldBlock {
