5e4365735a963c85ceddd533417d0e5802ee97c1
Ar7eniyan <ar7eniy.k@gmail.com>
2023-09-14 23:09:41 +0300

    feat: use sway ipc to receive workspace events

--- src/main.rs
+++ src/main.rs
@@ -11,21 +11,22 @@ use evdev_rs::ReadStatus;
 use evdev_rs::UInputDevice;
 
 #[derive(Debug)]
-enum AltTabEvent {
+enum WorkspaceSwitcherEvent {
     Tab,
     EndMeta,
+    SwayWsEvent(Box<swayipc::WorkspaceEvent>),
 }
 
 struct AltTabInterceptor {
     in_device: Device,
     out_device: UInputDevice,
-    evt_tx: Sender<AltTabEvent>,
+    evt_tx: Sender<WorkspaceSwitcherEvent>,
     was_tab: bool,
     meta_pressed: bool,
 }
 
 impl AltTabInterceptor {
-    fn new(in_device_path: &str, evt_tx: Sender<AltTabEvent>) -> io::Result<Self> {
+    fn new(in_device_path: &str, evt_tx: Sender<WorkspaceSwitcherEvent>) -> io::Result<Self> {
         let file = OpenOptions::new()
             .read(true)
             .write(true)
@@ -73,7 +74,7 @@ impl AltTabInterceptor {
             (EV_KEY(KEY_LEFTMETA) | EV_KEY(KEY_RIGHTMETA), 0 | 1) => {
                 self.meta_pressed = evt.value == 1;
                 if evt.value == 0 && self.was_tab {
-                    self.evt_tx.send(AltTabEvent::EndMeta).unwrap();
+                    self.evt_tx.send(WorkspaceSwitcherEvent::EndMeta).unwrap();
                     self.was_tab = false;
                 }
                 Some(evt)
@@ -81,7 +82,7 @@ impl AltTabInterceptor {
             (EV_KEY(KEY_TAB), 1) => {
                 if self.meta_pressed {
                     self.was_tab = true;
-                    self.evt_tx.send(AltTabEvent::Tab).unwrap();
+                    self.evt_tx.send(WorkspaceSwitcherEvent::Tab).unwrap();
                     None
                 } else {
                     Some(evt)
@@ -93,11 +94,11 @@ impl AltTabInterceptor {
 }
 
 struct AltTabWorkspaceSwitcher {
-    evt_rx: Receiver<AltTabEvent>,
+    evt_rx: Receiver<WorkspaceSwitcherEvent>,
 }
 
 impl AltTabWorkspaceSwitcher {
-    fn new(evt_rx: Receiver<AltTabEvent>) -> Self {
+    fn new(evt_rx: Receiver<WorkspaceSwitcherEvent>) -> Self {
         Self { evt_rx }
     }
 
@@ -110,9 +111,9 @@ impl AltTabWorkspaceSwitcher {
 }
 
 fn main() {
-    let (tx, rx) = std::sync::mpsc::channel::<AltTabEvent>();
+    let (tx, rx) = std::sync::mpsc::channel::<WorkspaceSwitcherEvent>();
 
-    let mut interceptor = AltTabInterceptor::new("/dev/input/event12", tx).unwrap();
+    let mut interceptor = AltTabInterceptor::new("/dev/input/event12", tx.clone()).unwrap();
     println!(
         "uinput device: {}",
         interceptor.out_device.devnode().unwrap_or("none")
@@ -129,6 +130,34 @@ fn main() {
         .name("interceptor".to_string())
         .spawn(move || interceptor.run())
         .unwrap();
+
+    let conn = swayipc::Connection::new().unwrap();
+    let evt_iter = conn.subscribe([swayipc::EventType::Workspace]).unwrap();
+
+    // sway workspace event types:
+    // init - TODO
+    // empty - TODO
+    // focus - TODO
+    // move - (?) ignore
+    // rename - ignore
+    // urgent - ignore
+    // reload - ignore
+
+    // Forward sway workspace events to the switcher thread
+    // Should I make this a separate thread?
+    for evt in evt_iter {
+        match evt {
+            Ok(swayipc::Event::Workspace(evt)) => {
+                tx.send(WorkspaceSwitcherEvent::SwayWsEvent(evt)).unwrap()
+            },
+            Err(e) => {
+                println!("Sway event stream error: {:?}", e);
+            },
+            _ => {
+
+            },
+        }
+    }
 
     // Should I do something else here?
     std::thread::park();
