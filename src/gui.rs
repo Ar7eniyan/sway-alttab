2be811b2507fe82a2e98c0470691672011ce3bc3
Ar7eniyan <ar7eniy.k@gmail.com>
2023-10-15 14:10:52 +0300

    feat: render someting with tiny_skia

--- src/gui.rs
+++ src/gui.rs
@@ -1,5 +1,11 @@
+// TODO:
+// 1. General rendering infrastructure
+// 2. Utilities for rendering rounded rectangles
+// 3. Draw text into a tiny_skia pixmap using fontdue
+// 4. Make a way to send the events from WorkspaceSwitcher to here
+
 use std::{
-    io::Seek,
+    io::{Seek, Write},
     os::{
         fd::{AsFd, BorrowedFd},
         unix::prelude::FileExt,
@@ -14,6 +20,8 @@ use wayland_client::{
     },
     Connection, Dispatch, EventQueue, QueueHandle,
 };
+
+use tiny_skia;
 
 use wayland_protocols_wlr::layer_shell::v1::client::{zwlr_layer_shell_v1, zwlr_layer_surface_v1};
 
@@ -47,6 +55,22 @@ pub struct Gui {
     event_queue: EventQueue<WaylandState>,
 }
 
+impl Dispatch<wl_shm::WlShm, ()> for WaylandState {
+    fn event(
+        state: &mut Self,
+        _: &wl_shm::WlShm,
+        event: <wl_shm::WlShm as wayland_client::Proxy>::Event,
+        _: &(),
+        _: &Connection,
+        _: &wayland_client::QueueHandle<Self>,
+    ) {
+        match event {
+            wl_shm::Event::Format { format } => println!("{:?}", format),
+            _ => {}
+        }
+    }
+}
+
 impl Dispatch<zwlr_layer_surface_v1::ZwlrLayerSurfaceV1, ()> for WaylandState {
     fn event(
         state: &mut Self,
@@ -54,7 +78,7 @@ impl Dispatch<zwlr_layer_surface_v1::ZwlrLayerSurfaceV1, ()> for WaylandState {
         event: <zwlr_layer_surface_v1::ZwlrLayerSurfaceV1 as wayland_client::Proxy>::Event,
         _: &(),
         _: &Connection,
-        _: &wayland_client::QueueHandle<Self>,
+        qh: &wayland_client::QueueHandle<Self>,
     ) {
         match event {
             zwlr_layer_surface_v1::Event::Configure {
@@ -65,11 +89,33 @@ impl Dispatch<zwlr_layer_surface_v1::ZwlrLayerSurfaceV1, ()> for WaylandState {
                 println!("Received wlr_layer_surface_v1::configure: serial = {serial}, size = {width}x{height}");
                 layer_surface.ack_configure(serial);
 
-                state.surface_buffer_file.seek(std::io::SeekFrom::Start(0)).unwrap();
-                draw(&mut state.surface_buffer_file, (Gui::WIDTH as u32, Gui::HEIGHT as u32), 100);
-                let buf = state.shm_pool.create_buffer(0, Gui::WIDTH, Gui::HEIGHT, Gui::STRIDE, wl_shm::Format::Argb8888, &state.queue_handle, ());
+                let buffer: &mut [u32] = &mut [0; (Gui::WIDTH * Gui::HEIGHT) as usize][..];
+                draw_skia(buffer, (Gui::WIDTH as u32, Gui::HEIGHT as u32));
+
+                state
+                    .surface_buffer_file
+                    .seek(std::io::SeekFrom::Start(0))
+                    .unwrap();
+                for rgba in buffer.iter() {
+                    let argb = (rgba >> 8) + (rgba << 24);
+                    state
+                        .surface_buffer_file
+                        .write(&argb.to_le_bytes())
+                        .unwrap();
+                }
+
+                let buf = state.shm_pool.create_buffer(
+                    0,
+                    Gui::WIDTH,
+                    Gui::HEIGHT,
+                    Gui::STRIDE,
+                    wl_shm::Format::Abgr8888,
+                    qh,
+                    (),
+                );
                 state.surface.attach(Some(&buf), 0, 0);
                 state.surface.damage(0, 0, Gui::WIDTH, Gui::HEIGHT);
+                state.surface.commit();
                 layer_surface.set_size(Gui::WIDTH as u32, Gui::HEIGHT as u32);
                 state.surface.commit();
             }
@@ -121,8 +167,6 @@ impl Gui {
             (),
         );
 
-        draw(&mut file, (Self::WIDTH as u32, Self::HEIGHT as u32), 100);
-
         let surface = compositor.create_surface(&qh, ());
         let layer_surface = layer_shell.get_layer_surface(
             &surface,
@@ -135,13 +179,13 @@ impl Gui {
         surface.commit();
 
         let mut window = WaylandState {
-                surface,
-                shm_pool,
-                layer_surface,
-                surface_buffer,
-                surface_buffer_file: file,
-                queue_handle: qh,
-            };
+            surface,
+            shm_pool,
+            layer_surface,
+            surface_buffer,
+            surface_buffer_file: file,
+            queue_handle: qh,
+        };
 
         event_queue.roundtrip(&mut window).unwrap();
 
@@ -157,7 +201,7 @@ impl Gui {
         let mut t = 0.0;
 
         loop {
-            self.redraw(t as u32);
+            self.redraw_skia(t as u32);
             self.event_queue
                 .blocking_dispatch(&mut self.window)
                 .unwrap();
@@ -167,9 +211,54 @@ impl Gui {
     }
 
     pub fn redraw(&mut self, t: u32) {
-        self.window.surface_buffer_file.seek(std::io::SeekFrom::Start(0)).unwrap();
-        draw(&mut self.window.surface_buffer_file, (Self::WIDTH as u32, Self::HEIGHT as u32), t);
-        let buf = self.window.shm_pool.create_buffer(0, Self::WIDTH, Self::HEIGHT, Self::STRIDE, wl_shm::Format::Argb8888, &self.event_queue.handle(), ());
+        self.window
+            .surface_buffer_file
+            .seek(std::io::SeekFrom::Start(0))
+            .unwrap();
+        draw(
+            &mut self.window.surface_buffer_file,
+            (Self::WIDTH as u32, Self::HEIGHT as u32),
+            t,
+        );
+        let buf = self.window.shm_pool.create_buffer(
+            0,
+            Self::WIDTH,
+            Self::HEIGHT,
+            Self::STRIDE,
+            wl_shm::Format::Abgr8888,
+            &self.event_queue.handle(),
+            (),
+        );
+        self.window.surface.attach(Some(&buf), 0, 0);
+        self.window.surface.damage(0, 0, Self::WIDTH, Self::HEIGHT);
+        self.window.surface.commit();
+    }
+
+    pub fn redraw_skia(&mut self, t: u32) {
+        let buffer: &mut [u32] = &mut [0; (Self::WIDTH * Self::HEIGHT) as usize][..];
+        draw_skia(buffer, (Self::WIDTH as u32, Self::HEIGHT as u32));
+
+        self.window
+            .surface_buffer_file
+            .seek(std::io::SeekFrom::Start(0))
+            .unwrap();
+        for rgba in buffer.iter() {
+            let argb = (rgba >> 8) + (rgba << 24);
+            self.window
+                .surface_buffer_file
+                .write(&argb.to_le_bytes())
+                .unwrap();
+        }
+
+        let buf = self.window.shm_pool.create_buffer(
+            0,
+            Self::WIDTH,
+            Self::HEIGHT,
+            Self::STRIDE,
+            wl_shm::Format::Abgr8888,
+            &self.event_queue.handle(),
+            (),
+        );
         self.window.surface.attach(Some(&buf), 0, 0);
         self.window.surface.damage(0, 0, Self::WIDTH, Self::HEIGHT);
         self.window.surface.commit();
@@ -182,15 +271,48 @@ fn draw(tmp: &mut std::fs::File, (buf_x, buf_y): (u32, u32), mut t: u32) {
     t = t % 0xff;
     for y in 0..buf_y {
         for x in 0..buf_x {
-            let r = t * min(((buf_x - x) * 0xFF) / (buf_x), ((buf_y - y) * 0xFF) / (buf_y)) / 0xff;
+            let r = t * min(
+                ((buf_x - x) * 0xFF) / (buf_x),
+                ((buf_y - y) * 0xFF) / (buf_y),
+            ) / 0xff;
             let g = t * min((x * 0xFF) / (buf_x), ((buf_y - y) * 0xFF) / (buf_y)) / 0xff;
             let b = t * min(((buf_x - x) * 0xFF) / (buf_x), (y * 0xFF) / (buf_y)) / 0xff;
 
-            let color = (t << 24) + ((r & 0xFF) << 16) + ((g & 0xFF) << 8) + (b & 0xFF);
+            let color = ((r & 0xFF) << 24) + ((g & 0xFF) << 16) + ((b & 0xFF) << 8) + (t & 0xFF);
             buf.write_all(&color.to_ne_bytes()).unwrap();
         }
     }
     buf.flush().unwrap();
+}
+
+fn draw_skia(buffer: &mut [u32], (width, height): (u32, u32)) {
+    // Safety: the buffer is accessed only through `bytes` during the rendering
+    // and alignment is not a problem with u8
+    let bytes =
+        unsafe { std::slice::from_raw_parts_mut(buffer.as_ptr() as *mut u8, buffer.len() * 4) };
+    let mut pixmap = tiny_skia::PixmapMut::from_bytes(bytes, width, height).unwrap();
+
+    let paint = tiny_skia::Paint {
+        shader: tiny_skia::Shader::SolidColor(tiny_skia::Color::from_rgba8(0xFF, 0x00, 0xFF, 0x11)),
+        ..Default::default()
+    };
+    let path = tiny_skia::PathBuilder::from_rect(
+        tiny_skia::Rect::from_xywh(
+            width as f32 * 0.1,
+            height as f32 * 0.1,
+            width as f32 * 0.8,
+            height as f32 * 0.8,
+        )
+        .unwrap(),
+    );
+
+    pixmap.fill_path(
+        &path,
+        &paint,
+        Default::default(),
+        Default::default(),
+        Default::default(),
+    );
 }
 
 impl wayland_client::Dispatch<wl_registry::WlRegistry, GlobalListContents> for WaylandState {
@@ -208,7 +330,7 @@ impl wayland_client::Dispatch<wl_registry::WlRegistry, GlobalListContents> for W
 
 delegate_noop!(WaylandState: ignore wl_compositor::WlCompositor);
 delegate_noop!(WaylandState: ignore wl_surface::WlSurface);
-delegate_noop!(WaylandState: ignore wl_shm::WlShm);
+// delegate_noop!(WaylandState: ignore wl_shm::WlShm);
 delegate_noop!(WaylandState: ignore wl_shm_pool::WlShmPool);
 delegate_noop!(WaylandState: ignore wl_buffer::WlBuffer);
 delegate_noop!(WaylandState: ignore zwlr_layer_shell_v1::ZwlrLayerShellV1);
